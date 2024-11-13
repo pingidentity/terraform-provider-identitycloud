3a3d828a494dd28abc7a8f78c97e88ab76f72b37
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2024-11-13 11:49:18 -0600

    Fix variable name

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -98,7 +98,7 @@ func (p *identityCloudProvider) Configure(ctx context.Context, req provider.Conf
 	}
 
 	if envFqdn == "" {
-		resp.Diagnostics.AddAttributeError(path.Root("tenant_environment_fqdn"), providererror.InvalidProviderConfiguration, "tenant_environment_fqdn provider attribute is required. If not set in the provider configuration, it can be set with the `PINGAIC_TF_TENANT_ENV_FQDN` environment variable.")
+		resp.Diagnostics.AddAttributeError(path.Root("tenant_environment_fqdn"), providererror.InvalidProviderConfigurationError, "tenant_environment_fqdn provider attribute is required. If not set in the provider configuration, it can be set with the `PINGAIC_TF_TENANT_ENV_FQDN` environment variable.")
 	}
 	// else {
 	// TODO validate the FQDN
