ebb0a41c14f5cbac896a5cfe9d1c6c09eb31546c
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2024-11-18 16:09:39 -0600

    Cleanup for merge

--- internal/resource/promotion/promotion_lock_resource_gen.go
+++ internal/resource/promotion/promotion_lock_resource_gen.go
@@ -4,7 +4,6 @@ package promotion
 
 import (
 	"context"
-	"io"
 	"net/http"
 	"time"
 
@@ -14,7 +13,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	client "github.com/pingidentity/identitycloud-go-client/identitycloud"
 	"github.com/pingidentity/terraform-provider-identitycloud/internal/auth"
 	"github.com/pingidentity/terraform-provider-identitycloud/internal/providererror"
@@ -225,10 +223,8 @@ func (r *promotionLockResource) Create(ctx context.Context, req resource.CreateR
 	var httpResp *http.Response
 	var err error
 	finalErr := retry.Do(ctx, retries, func(ctx context.Context) error {
-		tflog.Warn(ctx, "Environment lock attempt")
 		_, httpResp, err = r.apiClient.PromotionAPI.LockExecute(apiCreateRequest)
 		if err != nil && httpResp != nil && httpResp.StatusCode == 409 {
-			tflog.Warn(ctx, "Environment lock retryable error: "+err.Error())
 			return retry.RetryableError(err)
 		}
 		return err
@@ -314,16 +310,8 @@ func (r *promotionLockResource) Delete(ctx context.Context, req resource.DeleteR
 	var httpResp *http.Response
 	var err error
 	finalErr := retry.Do(ctx, retries, func(ctx context.Context) error {
-		tflog.Warn(ctx, "Environment unlock attempt")
 		_, httpResp, err = r.apiClient.PromotionAPI.Unlock(auth.AuthContext(ctx, r.accessToken, r.serviceAccountTokenSource), data.PromotionId.ValueString()).AcceptAPIVersion("protocol=1.0,resource=1.0").Execute()
 		if err != nil && httpResp != nil && httpResp.StatusCode == 409 {
-			defer httpResp.Body.Close()
-			body, bodyErr := io.ReadAll(httpResp.Body)
-			if bodyErr == nil {
-				tflog.Warn(ctx, "Environment unlock retryable error: "+err.Error()+", "+string(body))
-			} else {
-				tflog.Warn(ctx, "Environment unlock retryable error (body read failed): "+err.Error())
-			}
 			return retry.RetryableError(err)
 		}
 		return err
